a7292cc6742d3fdbe6c39e968488e993ff84d85f
Manish Patidar <MPatidar@wsgc.com>
2024-01-21 01:30:55 +0530

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Manish Patidar on 21/01/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
